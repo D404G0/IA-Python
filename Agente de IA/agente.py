b81ff04b3aebf3adf62dbc8caa5be7666e09ccbb
Diego Patiño <diegol-patinof@unilibre.edu.co>
2026-05-22 00:28:26 -0500

    Agente de Ia usando api de nvidia modelo gemma-3n-e4b-it

--- Agente de IA/agente.py
+++ Agente de IA/agente.py
@@ -1,6 +1,7 @@
 #---------------------Importar libreria---------------------#
 import requests
 import os
+import json
 from requests import Response
 from datos import url, token
 
@@ -16,26 +17,10 @@ headers = {
 messages = [
     {
         "role": "system", 
-        "content": "Eres un asistente que ayudaras a responder preguntas que te pidan, te limitaras estrictramente lo que te pidan sin agregar mas informacion"
+        "content": "Eres un asistente que ayudaras a responder preguntas que te pidan, te limitaras estritramente lo que te pidan sin agregar mas informacion"
     }
 ]
 
-#---------------------Funcion de herramientas---------------------#
-def list_files(directory = "."):
-    print("⚙️ Herramienta llamada: list_files")
-
-    try:
-        files = os.listdir(directory)
-        return{"files": files}
-    
-    except Exception as e:
-        return ("error": str(e))
-
-tools = [
-    {
-        
-    }
-]
 #---------------------Respuesta de modelo---------------------#
 while True:
 
@@ -55,20 +40,18 @@ while True:
     #-------------Conexion y respuesta de modelo de ia-------------#
     payload = {
         "model": "google/gemma-3n-e4b-it",
-        "messages": messages  
+        "messages": messages
     }
 
     response = requests.post(invoke_url, headers = headers, json = payload)
     datos = response.json()
-
+    
     if "choices" in datos:
         texto_ia = datos["choices"][0]["message"]["content"]
-        
         messages.append({"role": "assistant", "content": texto_ia})
 
         print(f'IA: \n {texto_ia}')
         print("-" * 30) 
-
 
     else:
         print("Hubo un error en la petición. Respuesta del servidor:")
